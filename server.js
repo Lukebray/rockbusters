e56d2358b7e08fad60bcbe4084ccf1c36df7f826
Lukebray <39488446+Lukebray@users.noreply.github.com>
2020-07-18 23:22:20 +0100

    test db

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 })
 
 // Connect to MongoDB
-mongoose.connect(process.env.DB_CONNECTION, {
+mongoose.connect("mongodb://luke:mkQxJXm83CuhUkay@cluster0-shard-00-00.lbs3u.mongodb.net:27017,cluster0-shard-00-01.lbs3u.mongodb.net:27017,cluster0-shard-00-02.lbs3u.mongodb.net:27017/rockbusters?ssl=true&replicaSet=atlas-w9nu7p-shard-0&authSource=admin&retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
@@ -27,6 +27,9 @@ mongoose.connect(process.env.DB_CONNECTION, {
 // Use routes
 app.use('/rockbusters', rockbustersRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server run at port ${PORT}`));
+//const PORT = process.env.PORT || 5000;
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+//app.listen(PORT, () => console.log(`Server run at port ${PORT}`));
